44353d521aa1ce0826c4ca4ba87e633b86f51a47
glennprays <glennsagala@gmail.com>
2025-10-30 14:33:22 +0700

    feat(build): integrate Tailwind CSS v4 and plugin support
    Add Tailwind CSS v4 and @tailwindcss/vite to dependencies. Update
    vite.config.ts to include Tailwind plugin. Import app.css in layout.
    Extend markup with Tailwind utility classes for fonts and backgrounds.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="font-graphik">documentation</h1>
+<p class="bg-test-300">
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+</p>
